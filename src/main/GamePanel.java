2310f7ae30f7d9120f4d2fe3c5e0eca7e56de224
Shepard Summers <115957978+shepardsummers@users.noreply.github.com>
2024-10-31 15:29:19 -0400

    Started tutorial

---
+++ src/main/GamePanel.java
@@ -0,0 +1,95 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import entity.Player;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable {
+	
+	// SCREEN SETTINGS
+	final int originalTileSize = 16; //16x16 pixels
+	final int scale = 3;
+	public final int tileSize = originalTileSize * scale; // 48x48 tile
+	
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = tileSize * maxScreenCol;
+	final int screenHeight = tileSize * maxScreenRow;
+	
+	// FPS
+	int FPS = 60;
+	
+	KeyHandler keyH = new KeyHandler();
+	Thread gameThread;
+	Player player = new Player(this,keyH);
+	
+	// Players default position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
+	
+	public GamePanel() {
+		
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+		
+	}
+	
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+		
+	}
+
+	@Override
+	public void run() {
+			
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		
+		while(gameThread != null) {
+						
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			lastTime = currentTime;
+			
+			if(delta >= 1) {
+				update();
+				repaint();
+				
+				delta--;
+			}	
+			
+		}
+		
+	}
+	
+	public void update() {
+		
+		player.update();
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D) g;
+		
+		player.draw(g2);
+		
+		g2.dispose();
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module three {
+	requires java.desktop;
+}
